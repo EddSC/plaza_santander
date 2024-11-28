f2170f365bba7112d1ebde00a70e5cfc50716703
Eder Coarita <eder_coarita@usmp.pe>
2024-11-28 11:38:38 -0500

    cambios

--- main.py
+++ main.py
@@ -5,12 +5,6 @@ from app.routers import auth_router
 from app.routers import health_router
 from app.routers import centros_comerciales_router
 from app.models import BaseModel
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
 
 
 BaseModel.set_session(db.session)
